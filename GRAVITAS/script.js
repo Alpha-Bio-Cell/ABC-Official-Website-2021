0000ab562dbab4339d82f8527290a6ff51a309dd
Anurag Mahajan <arm2002mahajan@gmail.com>
2022-09-19 12:30:50 +0530

    Gravitas

--- GRAVITAS/script.js
+++ GRAVITAS/script.js
@@ -1,15 +1,1 @@
-function addSpeaker() {
-    document.getElementById("speakerAbout").innerHTML = "I am speaker";
-}
-function addSponsor() {
-    document.getElementById("sponsorAbout").innerHTML = "Iam Sponsor";
-}
-
-function addCashFlow() {
-    document.getElementById("cashflowshow").innerHTML = "Cash Flow is here";
-}
-
-function addMerchandise() {
-    document.getElementById("merchandiseShow").innerHTML = "Merchandise is here";
-}
 
